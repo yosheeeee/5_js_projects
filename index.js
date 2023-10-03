4b80689bc6756183a4caf679bba4260e98cab571
yosheeeee <evantipin@edu.hse.com>
2023-10-03 22:42:23 +0500

    сделано бонусное задание

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-let slides = document.querySelectorAll('.slide')
 
-for (let slide of slides){
-    slide.addEventListener('click', event => {
-        ClearActiveClasses()
-        event.target.classList.add('active')
-    })
-}
-
-function ClearActiveClasses(){
+function slidesPlugin(numberOfSlide){
+    let slides = document.querySelectorAll('.slide')
+    slides[numberOfSlide].classList.add('active')
     for (let slide of slides){
-        slide.classList.remove('active')
+        slide.addEventListener('click', event => {
+            ClearActiveClasses()
+            event.target.classList.add('active')
+        })
+    }
+
+    function ClearActiveClasses(){
+        for (let slide of slides){
+            slide.classList.remove('active')
+        }
     }
 }
+
+slidesPlugin(2)
